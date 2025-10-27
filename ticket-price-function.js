0d1f072934474d7ccf02309ffa01250862119ac7
daniels <danielsv2008@gmail.com>
2025-10-27 11:25:00 +0200

    added file 'ticket-price-function'

---
+++ ticket-price-function.js
@@ -0,0 +1,15 @@
+let visitorAge = 12
+let isStudent = true;
+
+    if(visitorAge <= 6 + isStudent){
+        console.log("ieeja bezmaksas")
+    } else {
+        console.log("ieeja ir 10EUR")
+    }
+
+
+    if(isStudent = true) {
+        console.log("ieeja 5EUR")
+    } else {
+        console.log("ieeja 10EUR")
+    }
