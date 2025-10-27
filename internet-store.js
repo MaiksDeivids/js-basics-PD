961063b778aa91c93a3098e8def1c476d71df321
daniels <danielsv2008@gmail.com>
2025-10-27 11:40:11 +0200

    added file 'internet-store.js'

---
+++ internet-store.js
@@ -0,0 +1,9 @@
+let saraksts = [
+    {prece: "TV", price: 450, kategorija: "elektronika", daudzums: 50},
+    {prece: "laptop", price: 700, kategorija: "elektronika", daudzums: 30},
+    {prece: "krekls", price: 20, kategorija: "Apģērbi", daudzums: 100},
+    {prece: "bikses", price: 35, kategorija: "Apģērbi", daudzums: 120},
+
+]
+
+console.log(saraksts)
